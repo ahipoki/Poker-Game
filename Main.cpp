fd3f8314368e8ee1d6a04108b62b331ae262e476
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-15 09:08:56 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -393,315 +393,315 @@ void convertCard() {
     p1C2C = "King of Diamonds"; 
   }
   if (p2C1 == 1) {
-    p1C2C = "Ace of Spades";
+    p2C1C = "Ace of Spades";
   }  
   else if (p2C1 == 2) {
-    p1C2C = "2 of Spades";
+    p2C1C = "2 of Spades";
   }
   else if (p2C1 == 3) {
-    p1C2C = "3 of Spades";
+    p2C1C = "3 of Spades";
   }
   else if (p2C1 == 4) {
-    p1C2C = "4 of Spades";
+    p2C1C = "4 of Spades";
   }
   else if (p2C1 == 5) {
-    p1C2C = "5 of Spades";
+    p2C1C = "5 of Spades";
   }
   else if (p2C1 == 6) {
-    p1C2C = "6 of Spades";
+    p2C1C = "6 of Spades";
   }
   else if (p2C1 == 7) {
-    p1C2C = "7 of Spades";
+    p2C1C = "7 of Spades";
   }
   else if (p2C1 == 8) {
-    p1C2C = "8 of Spades";
+    p2C1C = "8 of Spades";
   }
   else if (p2C1 == 9) {
-    p1C2C = "9 of Spades";
+    p2C1C = "9 of Spades";
   }
   else if (p2C1 == 10) {
-    p2C1C = "10 of Spades";
-  }
-  else if (p2C1 == 11) {
-    p2C1C = "Jack of Spades";
-  }
-  else if (p2C1 == 12) {
-    p1C2C = "Queen of Spades";
-  }
-  else if (p2C1 == 13) {
-    p1C2C = "King of Spades";
-  }
-  else if (p2C1 == 14) {
-    p1C2C = "Ace of Clubs";
-  }
-  else if (p2C1 == 15) {
-    p1C2C = "2 of Clubs";
-  }
-  else if (p2C1 == 16) {
-    p1C2C = "3 of Clubs";
-  }
-  else if (p2C1 == 17) {
-    p1C2C = "4 of Clubs";
-  }
-  else if (p2C1 == 18) {
-    p1C2C = "5 of Clubs";
-  }
-  else if (p2C1 == 19) {
-    p1C2C = "6 of Clubs";
-  }
-  else if (p2C1 == 20) {
-    p1C2C = "7 of Clubs";
-  }
-  else if (p2C1 == 20) {
-    p1C2C = "8 of Clubs";
-  }
-  else if (p2C1 == 21) {
-    p1C2C = "9 of Clubs";
-  }
-  else if (p2C1 == 22) {
-    p1C2C = "10 of Clubs";
-  }
-  else if (p2C1 == 23) {
-    p1C2C = "Jack of Clubs";
-  }
-  else if (p2C1 == 24) {
-    p1C2C = "Queen of Clubs";
-  }
-  else if (p2C1 == 25) {
-    p1C2C = "King of Clubs";
-  }
-  else if (p2C1 == 26) {
-    p1C2C = "Ace of Hearts";
-  }
-  else if (p2C1 == 27) {
-    p1C2C = "2 of Hearts";
-  }
-  else if (p2C1 == 28) {
-    p1C2C = "3 of Hearts";
-  }
-  else if (p2C1 == 29) {
-    p1C2C = "4 of Hearts";
-  }
-  else if (p2C1 == 30) {
-    p1C2C = "5 of Hearts";
-  }
-  else if (p2C1 == 31) {
-    p1C2C = "6 of Hearts";
-  }
-  else if (p2C1 == 32) {
-    p1C2C = "7 of Hearts";
-  }
-  else if (p2C1 == 33) {
-    p1C2C = "8 of Hearts";
-  }
-  else if (p2C1 == 34) {
-    p1C2C = "9 of Hearts";
-  }
-  else if (p2C1 == 35) {
-    p1C2C = "10 of Hearts";
-  }
-  else if (p2C1 == 36) {
-    p1C2C = "Jack of Hearts";
-  }
-  else if (p2C1 == 37) {
-    p1C2C = "Queen of Hearts";
-  }
-  else if (p2C1 == 38) {
-    p1C2C = "King of Hearts";
-  }
-  else if (p2C1 == 39) {
-    p1C2C = "Ace of Diamonds";
-  }
-  else if (p2C1 == 40) {
-    p1C2C = "2 of Diamonds";
-  }
-  else if (p2C1 == 41) {
-    p1C2C = "3 of Diamonds";
-  }
-  else if (p2C1 == 42) {
-    p1C2C = "4 of Diamonds";
-  }
-  else if (p2C1 == 43) {
-    p1C2C = "5 of Diamonds";
-  }
-  else if (p2C1 == 44) {
-    p1C2C = "6 of Diamonds";
-  }
-  else if (p2C1 == 45) {
-    p1C2C = "7 of Diamonds";
-  }
-  else if (p2C1 == 46) {
-    p1C2C = "8 of Diamonds";
-  }
-  else if (p2C1 == 47) {
-    p1C2C = "9 of Diamonds";
-  }
-  else if (p2C1 == 48) {
-    p1C2C = "10 of Diamonds";
-  }
-  else if (p2C1 == 49) {
-    p1C2C = "Jack of Diamonds";
-  }
-  else if (p2C1 == 50) {
-    p1C2C = "Queen of Diamonds";
-  }
-  else {
-    p1C2C = "King of Diamonds"; 
-  }
-  if (p2C2 == 1) {
-    p1C2C = "Ace of Spades";
-  }  
-  else if (p2C2 == 2) {
-    p1C2C = "2 of Spades";
-  }
-  else if (p2C2 == 3) {
-    p1C2C = "3 of Spades";
-  }
-  else if (p2C2 == 4) {
-    p1C2C = "4 of Spades";
-  }
-  else if (p2C2 == 5) {
-    p1C2C = "5 of Spades";
-  }
-  else if (p2C2 == 6) {
-    p1C2C = "6 of Spades";
-  }
-  else if (p2C2 == 7) {
-    p1C2C = "7 of Spades";
-  }
-  else if (p2C2 == 8) {
-    p1C2C = "8 of Spades";
-  }
-  else if (p2C2 == 9) {
-    p1C2C = "9 of Spades";
-  }
-  else if (p2C2 == 10) {
     p1C2C = "10 of Spades";
   }
-  else if (p2C2 == 11) {
+  else if (p2C1 == 11) {
     p1C2C = "Jack of Spades";
   }
-  else if (p2C2 == 12) {
-    p1C2C = "Queen of Spades";
+  else if (p2C1 == 12) {
+    p2C1C = "Queen of Spades";
   }
-  else if (p2C2 == 13) {
-    p1C2C = "King of Spades";
+  else if (p2C1 == 13) {
+    p2C1C = "King of Spades";
   }
-  else if (p2C2 == 14) {
-    p1C2C = "Ace of Clubs";
+  else if (p2C1 == 14) {
+    p2C1C = "Ace of Clubs";
   }
-  else if (p2C2 == 15) {
-    p1C2C = "2 of Clubs";
+  else if (p2C1 == 15) {
+    p2C1C = "2 of Clubs";
   }
-  else if (p2C2 == 16) {
-    p1C2C = "3 of Clubs";
+  else if (p2C1 == 16) {
+    p2C1C = "3 of Clubs";
   }
-  else if (p2C2 == 17) {
-    p1C2C = "4 of Clubs";
+  else if (p2C1 == 17) {
+    p2C1C = "4 of Clubs";
   }
-  else if (p2C2 == 18) {
-    p1C2C = "5 of Clubs";
+  else if (p2C1 == 18) {
+    p2C1C = "5 of Clubs";
   }
-  else if (p2C2 == 19) {
-    p1C2C = "6 of Clubs";
+  else if (p2C1 == 19) {
+    p2C1C = "6 of Clubs";
   }
-  else if (p2C2 == 20) {
-    p1C2C = "7 of Clubs";
+  else if (p2C1 == 20) {
+    p2C1C = "7 of Clubs";
   }
-  else if (p2C2 == 20) {
-    p1C2C = "8 of Clubs";
+  else if (p2C1 == 20) {
+    p2C1C = "8 of Clubs";
   }
-  else if (p2C2 == 21) {
-    p1C2C = "9 of Clubs";
+  else if (p2C1 == 21) {
+    p2C1C = "9 of Clubs";
   }
-  else if (p2C2 == 22) {
-    p1C2C = "10 of Clubs";
+  else if (p2C1 == 22) {
+    p2C1C = "10 of Clubs";
   }
-  else if (p2C2 == 23) {
-    p1C2C = "Jack of Clubs";
+  else if (p2C1 == 23) {
+    p2C1C = "Jack of Clubs";
   }
-  else if (p2C2 == 24) {
-    p1C2C = "Queen of Clubs";
+  else if (p2C1 == 24) {
+    p2C1C = "Queen of Clubs";
   }
-  else if (p2C2 == 25) {
-    p1C2C = "King of Clubs";
+  else if (p2C1 == 25) {
+    p2C1C = "King of Clubs";
   }
-  else if (p2C2 == 26) {
-    p1C2C = "Ace of Hearts";
+  else if (p2C1 == 26) {
+    p2C1C = "Ace of Hearts";
   }
-  else if (p2C2 == 27) {
-    p1C2C = "2 of Hearts";
+  else if (p2C1 == 27) {
+    p2C1C = "2 of Hearts";
   }
-  else if (p2C2 == 28) {
-    p1C2C = "3 of Hearts";
+  else if (p2C1 == 28) {
+    p2C1C = "3 of Hearts";
   }
-  else if (p2C2 == 29) {
-    p1C2C = "4 of Hearts";
+  else if (p2C1 == 29) {
+    p2C1C = "4 of Hearts";
   }
-  else if (p2C2 == 30) {
-    p1C2C = "5 of Hearts";
+  else if (p2C1 == 30) {
+    p2C1C = "5 of Hearts";
   }
-  else if (p2C2 == 31) {
-    p1C2C = "6 of Hearts";
+  else if (p2C1 == 31) {
+    p2C1C = "6 of Hearts";
   }
-  else if (p2C2 == 32) {
-    p1C2C = "7 of Hearts";
+  else if (p2C1 == 32) {
+    p2C1C = "7 of Hearts";
   }
-  else if (p2C2 == 33) {
-    p1C2C = "8 of Hearts";
+  else if (p2C1 == 33) {
+    p2C1C = "8 of Hearts";
   }
-  else if (p2C2 == 34) {
-    p1C2C = "9 of Hearts";
+  else if (p2C1 == 34) {
+    p2C1C = "9 of Hearts";
   }
-  else if (p2C2 == 35) {
-    p1C2C = "10 of Hearts";
+  else if (p2C1 == 35) {
+    p2C1C = "10 of Hearts";
   }
-  else if (p2C2 == 36) {
-    p1C2C = "Jack of Hearts";
+  else if (p2C1 == 36) {
+    p2C1C = "Jack of Hearts";
   }
-  else if (p2C2 == 37) {
-    p1C2C = "Queen of Hearts";
+  else if (p2C1 == 37) {
+    p2C1C = "Queen of Hearts";
   }
-  else if (p2C2 == 38) {
-    p1C2C = "King of Hearts";
+  else if (p2C1 == 38) {
+    p2C1C = "King of Hearts";
   }
-  else if (p2C2 == 39) {
-    p1C2C = "Ace of Diamonds";
+  else if (p2C1 == 39) {
+    p2C1C = "Ace of Diamonds";
   }
-  else if (p2C2 == 40) {
-    p1C2C = "2 of Diamonds";
+  else if (p2C1 == 40) {
+    p2C1C = "2 of Diamonds";
   }
-  else if (p2C2 == 41) {
-    p1C2C = "3 of Diamonds";
+  else if (p2C1 == 41) {
+    p2C1C = "3 of Diamonds";
   }
-  else if (p2C2 == 42) {
-    p1C2C = "4 of Diamonds";
+  else if (p2C1 == 42) {
+    p2C1C = "4 of Diamonds";
   }
-  else if (p2C2 == 43) {
-    p1C2C = "5 of Diamonds";
+  else if (p2C1 == 43) {
+    p2C1C = "5 of Diamonds";
   }
-  else if (p2C2 == 44) {
-    p1C2C = "6 of Diamonds";
+  else if (p2C1 == 44) {
+    p2C1C = "6 of Diamonds";
   }
-  else if (p2C2 == 45) {
-    p1C2C = "7 of Diamonds";
+  else if (p2C1 == 45) {
+    p2C1C = "7 of Diamonds";
   }
-  else if (p2C2 == 46) {
-    p1C2C = "8 of Diamonds";
+  else if (p2C1 == 46) {
+    p2C1C = "8 of Diamonds";
   }
-  else if (p2C2 == 47) {
-    p1C2C = "9 of Diamonds";
+  else if (p2C1 == 47) {
+    p2C1C = "9 of Diamonds";
   }
-  else if (p2C2 == 48) {
-    p1C2C = "10 of Diamonds";
+  else if (p2C1 == 48) {
+    p2C1C = "10 of Diamonds";
   }
-  else if (p2C2 == 49) {
-    p1C2C = "Jack of Diamonds";
+  else if (p2C1 == 49) {
+    p2C1C = "Jack of Diamonds";
   }
-  else if (p2C2 == 50) {
-    p1C2C = "Queen of Diamonds";
+  else if (p2C1 == 50) {
+    p2C1C = "Queen of Diamonds";
   }
   else {
-    p1C2C = "King of Diamonds"; 
+    p2C1C = "King of Diamonds"; 
+  }
+  if (p2C2 == 1) {
+    p2C2C = "Ace of Spades";
+  }  
+  else if (p2C2 == 2) {
+    p2C2C = "2 of Spades";
+  }
+  else if (p2C2 == 3) {
+    p2C2C = "3 of Spades";
+  }
+  else if (p2C2 == 4) {
+    p2C2C = "4 of Spades";
+  }
+  else if (p2C2 == 5) {
+    p2C2C = "5 of Spades";
+  }
+  else if (p2C2 == 6) {
+    p2C2C = "6 of Spades";
+  }
+  else if (p2C2 == 7) {
+    p2C2C = "7 of Spades";
+  }
+  else if (p2C2 == 8) {
+    p2C2C = "8 of Spades";
+  }
+  else if (p2C2 == 9) {
+    p2C2C = "9 of Spades";
+  }
+  else if (p2C2 == 10) {
+    p2C2C = "10 of Spades";
+  }
+  else if (p2C2 == 11) {
+    p2C2C = "Jack of Spades";
+  }
+  else if (p2C2 == 12) {
+    p2C2C = "Queen of Spades";
+  }
+  else if (p2C2 == 13) {
+    p2C2C = "King of Spades";
+  }
+  else if (p2C2 == 14) {
+    p2C2C = "Ace of Clubs";
+  }
+  else if (p2C2 == 15) {
+    p2C2C = "2 of Clubs";
+  }
+  else if (p2C2 == 16) {
+    p2C2C = "3 of Clubs";
+  }
+  else if (p2C2 == 17) {
+    p2C2C = "4 of Clubs";
+  }
+  else if (p2C2 == 18) {
+    p2C2C = "5 of Clubs";
+  }
+  else if (p2C2 == 19) {
+    p2C2C = "6 of Clubs";
+  }
+  else if (p2C2 == 20) {
+    p2C2C = "7 of Clubs";
+  }
+  else if (p2C2 == 20) {
+    p2C2C = "8 of Clubs";
+  }
+  else if (p2C2 == 21) {
+    p2C2C = "9 of Clubs";
+  }
+  else if (p2C2 == 22) {
+    p2C2C = "10 of Clubs";
+  }
+  else if (p2C2 == 23) {
+    p2C2C = "Jack of Clubs";
+  }
+  else if (p2C2 == 24) {
+    p2C2C = "Queen of Clubs";
+  }
+  else if (p2C2 == 25) {
+    p2C2C = "King of Clubs";
+  }
+  else if (p2C2 == 26) {
+    p2C2C = "Ace of Hearts";
+  }
+  else if (p2C2 == 27) {
+    p2C2C = "2 of Hearts";
+  }
+  else if (p2C2 == 28) {
+    p2C2C = "3 of Hearts";
+  }
+  else if (p2C2 == 29) {
+    p2C2C = "4 of Hearts";
+  }
+  else if (p2C2 == 30) {
+    p2C2C = "5 of Hearts";
+  }
+  else if (p2C2 == 31) {
+    p2C2C = "6 of Hearts";
+  }
+  else if (p2C2 == 32) {
+    p2C2C = "7 of Hearts";
+  }
+  else if (p2C2 == 33) {
+    p2C2C = "8 of Hearts";
+  }
+  else if (p2C2 == 34) {
+    p2C2C = "9 of Hearts";
+  }
+  else if (p2C2 == 35) {
+    p2C2C = "10 of Hearts";
+  }
+  else if (p2C2 == 36) {
+    p2C2C = "Jack of Hearts";
+  }
+  else if (p2C2 == 37) {
+    p2C2C = "Queen of Hearts";
+  }
+  else if (p2C2 == 38) {
+    p2C2C = "King of Hearts";
+  }
+  else if (p2C2 == 39) {
+    p2C2C = "Ace of Diamonds";
+  }
+  else if (p2C2 == 40) {
+    p2C2C = "2 of Diamonds";
+  }
+  else if (p2C2 == 41) {
+    p2C2C = "3 of Diamonds";
+  }
+  else if (p2C2 == 42) {
+    p2C2C = "4 of Diamonds";
+  }
+  else if (p2C2 == 43) {
+    p2C2C = "5 of Diamonds";
+  }
+  else if (p2C2 == 44) {
+    p2C2C = "6 of Diamonds";
+  }
+  else if (p2C2 == 45) {
+    p2C2C = "7 of Diamonds";
+  }
+  else if (p2C2 == 46) {
+    p2C2C = "8 of Diamonds";
+  }
+  else if (p2C2 == 47) {
+    p2C2C = "9 of Diamonds";
+  }
+  else if (p2C2 == 48) {
+    p2C2C = "10 of Diamonds";
+  }
+  else if (p2C2 == 49) {
+    p2C2C = "Jack of Diamonds";
+  }
+  else if (p2C2 == 50) {
+    p2C2C = "Queen of Diamonds";
+  }
+  else {
+    p2C2C = "King of Diamonds"; 
   }
 }
